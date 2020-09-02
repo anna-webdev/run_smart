b2fab0456abc17cb2f078ce05d289a1cd8d42443
Anna <segenyuk.work@gmail.com>
2020-09-03 00:55:04 +0500

    carousel

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+              }
+        ]
+      });
+  });
